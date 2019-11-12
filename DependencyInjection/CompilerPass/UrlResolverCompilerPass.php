ad4cb2096409700352323ea7bc13fb7bb1944a8a
Yusef Mohamadi <yusef.mohamadi@auto1.com>
2019-11-12 11:43:35 +0100

    RLO-2189: make some changes to use in admin-logistics

--- DependencyInjection/CompilerPass/UrlResolverCompilerPass.php
+++ DependencyInjection/CompilerPass/UrlResolverCompilerPass.php
@@ -5,7 +5,6 @@ namespace Auto1\ServiceAPIComponentsBundle\DependencyInjection\CompilerPass;
 use Auto1\ServiceAPIComponentsBundle\Exception\Core\ConfigurationException;
 use Auto1\ServiceAPIComponentsBundle\Service\UrlResolver\UrlResolverInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-//use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
@@ -14,7 +13,6 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class UrlResolverCompilerPass implements CompilerPassInterface
 {
-//    use PriorityTaggedServiceTrait;
 
     const RESOLVER_TAG_NAME = 'auto1.api.url_resolver';
     const METHOD_REGISTER_RESOLVER = 'registerResolver';
@@ -25,6 +23,22 @@ class UrlResolverCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
+
+        $chainResolverDefinition = $container->getDefinition(self::SERVICE_CHAIN_RESOLVER);
+
+        /** @var Reference $resolver */
+        foreach ($this->findAndSortTaggedServices(self::RESOLVER_TAG_NAME, $container) as $resolver) {
+            $definition = $container->getDefinition((string) $resolver);
+            if (!is_a($definition->getClass(), UrlResolverInterface::class, true)) {
+                throw new ConfigurationException(
+                    sprintf('%s should be instance of %s', self::RESOLVER_TAG_NAME, UrlResolverInterface::class)
+                );
+            }
+
+            $chainResolverDefinition->addMethodCall(self::METHOD_REGISTER_RESOLVER, [$resolver]);
+        }
+
+
         $warmers = array();
         foreach ($container->findTaggedServiceIds('kernel.cache_warmer') as $id => $attributes) {
             $priority = isset($attributes[0]['priority']) ? $attributes[0]['priority'] : 0;
@@ -33,5 +47,28 @@ class UrlResolverCompilerPass implements CompilerPassInterface
 
         krsort($warmers);
         $warmers = call_user_func_array('array_merge', $warmers);
+        $chainResolverDefinition->addMethodCall(self::METHOD_REGISTER_RESOLVER, [$resolver]);
+    }
+
+    /**
+     * @param $tagName
+     * @param ContainerBuilder $container
+     * @return array
+     */
+    private function findAndSortTaggedServices($tagName, ContainerBuilder $container)
+    {
+        $services = [];
+
+        foreach ($container->findTaggedServiceIds($tagName, true) as $serviceId => $attributes) {
+            $priority = isset($attributes[0]['priority']) ? $attributes[0]['priority'] : 0;
+            $services[$priority][] = new Reference($serviceId);
+        }
+
+        if ($services) {
+            krsort($services);
+            $services = array_merge(...$services);
+        }
+
+        return $services;
     }
 }
